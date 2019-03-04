1bfd9bfdc061bd77321a6eb02c3538eb3f1cec6b
David Cicoria <46363078+DavidCico@users.noreply.github.com>
2019-03-04 14:41:31 +0800

    Add files via upload

---
+++ Open_conversion_data.py
@@ -0,0 +1,78 @@
+import csv
+from math import sqrt
+
+
+## Load a CSV file
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = csv.reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
+
+## Convert string column to float
+def str_column_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+
+## Convert string column to integer
+def str_column_to_int(dataset, column):
+	class_values = [row[column] for row in dataset]
+	unique = set(class_values)
+	lookup = dict()
+	for i, value in enumerate(unique):
+		lookup[value] = i
+	for row in dataset:
+		row[column] = lookup[row[column]]
+	return lookup
+
+
+##### Normalize Data ###########
+
+# Find the min and max values for each column
+
+def dataset_minmax(dataset):
+	minmax = list()
+	for i in range(len(dataset[0])):
+		colvalues = [row[i] for row in dataset]
+		min_value = min(colvalues) 
+		max_value = max(colvalues)
+		minmax.append([min_value, max_value])
+	return minmax
+
+# Normalize the dataset except last row for classification values
+def Normalize_Dataset(dataset, minmax):
+	for row in dataset:
+		for i in range(len(row)-1):
+			row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+
+
+#### Standardize Data ######
+
+# calculate column means
+def column_means(dataset):
+	means = [0 for i in range(len(dataset[0]))]
+	for i in range(len(dataset[0])):
+		col_values = [row[i] for row in dataset]
+		means[i] = sum(col_values) / float(len(dataset))
+	return means
+
+# calculate column standard deviations
+def column_stdevs(dataset, means):
+	stdevs = [0 for i in range(len(dataset[0]))]
+	for i in range(len(dataset[0])):
+		variance = [pow(row[i]-means[i], 2) for row in dataset]
+		stdevs[i] = sum(variance)
+		stdevs = [sqrt(x/(float(len(dataset)-1))) for x in stdevs]
+	return stdevs
+
+# Standardize the dataset
+def Standardize_Dataset(dataset, means, stdevs):
+	for row in dataset:
+		for i in range(len(row)):
+			row[i] = (row[i] - means[i]) / stdevs[i]
+
+
---
+++ Split_dataset.py
@@ -0,0 +1,25 @@
+# Example of Splitting a Contrived Dataset into Train and Test
+from random import randrange
+
+# Split a dataset into a train and test set
+def train_test_split(dataset, split):
+    train = list()
+    train_size = split * len(dataset)
+    dataset_copy = list(dataset)
+    while len(train) < train_size:
+        index = randrange(len(dataset_copy))
+        train.append(dataset_copy.pop(index))
+    return train, dataset_copy
+
+# Split a dataset into $k$ folds
+def cross_validation_split(dataset, folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / folds)
+    for i in range(folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
